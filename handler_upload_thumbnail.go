488065f7f9b78c1df86ca54ac007436fe62d5fe1
kairos4213 <jamesdkairos@gmail.com>
2025-01-02 20:05:30 -0600

    update(handlerUploadThumbnail): no longer allows any file type to be uploaded for thumbnails
    uses mime.ParseMediaType to get mediaType or returns an error
    checks if mediaType is 'image/jpeg' or 'image/png' and responds with err if conditions are not met

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"mime"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -45,7 +46,16 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	}
 	defer file.Close()
 
-	mediaType := header.Header.Get("Content-Type")
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "error getting media type", err)
+		return
+	}
+
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "invalid media type", nil)
+		return
+	}
 	fileExt := strings.Split(mediaType, "/")[1]
 
 	newFileName := fmt.Sprintf("%s.%s", videoID, fileExt)
